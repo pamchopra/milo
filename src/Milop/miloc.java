efc2b058d3b112addf56631467172da3f8dd6023
Poona_000 <Poona_000@cfamily.fios-router.home>
2021-04-08 13:55:26 -0400

    first commit

---
+++ src/Milop/miloc.java
@@ -0,0 +1,21 @@
+package Milop;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+public class miloc {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.com/");
+
+		//C:\Users\Poona_000\OneDrive\Documents\selenium
+		 //
+	}
+
+}
